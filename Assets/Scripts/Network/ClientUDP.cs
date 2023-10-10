7c35fa4dbf81eb578498e02c8365ca33e41d4826
Erik Martín Garzón <erikmarting1200@gmail.com>
2023-10-10 12:22:41 +0200

    UI base

--- Assets/Scripts/Network/ClientUDP.cs
+++ Assets/Scripts/Network/ClientUDP.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine.UI;
 
-public class ClientUDP : MonoBehaviour
+public class UDPClient : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private UdpClient client;
+    private IPEndPoint serverEndPoint;
+
+    static public InputField ipInputField;
+    static public InputField usernameInputField;
+
+    private string serverIP;
+    private string playerName;
+
+
+    private void Start()
     {
-        
+        client = new UdpClient();
+        //serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetIP(string inputIP)
+    {
+        serverIP = inputIP;
+        Debug.Log(inputIP);
+    }
+
+    public void SetPlayerName(string inputName)
+    {
+        playerName = inputName;
+    }
+
+    public void ConnectToServer()
     {
         
+        string message = "CONNECT|" + playerName;
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        Debug.Log("Trying connection");
+        Debug.Log(serverIP);
+        Debug.Log(playerName);
+        //client.Send(data, data.Length, serverEndPoint);
     }
 }
