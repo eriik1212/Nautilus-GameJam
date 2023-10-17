e67aa97a676419c6336e0df906e77038f4db2960
David Boces Obis <79161189+VoZeS@users.noreply.github.com>
2023-10-17 10:57:00 +0200

    UDP y TCP from links

--- Assets/Scripts/Network/ClientUDP.cs
+++ Assets/Scripts/Network/ClientUDP.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -6,41 +7,81 @@ using UnityEngine.UI;
 
 public class UDPClient : MonoBehaviour
 {
-    private UdpClient client;
-    private IPEndPoint serverEndPoint;
+    // -------------------------------------------------- FIRST TRY
+    //private UdpClient client;
+    //private IPEndPoint serverEndPoint;
 
-    static public InputField ipInputField;
-    static public InputField usernameInputField;
+    //static public InputField ipInputField;
+    //static public InputField usernameInputField;
 
-    private string serverIP;
-    private string playerName;
+    //private string serverIP;
+    //private string playerName;
 
 
-    private void Start()
+    //private void Start()
+    //{
+    //    client = new UdpClient();
+    //    //serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
+    //}
+
+    //public void SetIP(string inputIP)
+    //{
+    //    serverIP = inputIP;
+    //    Debug.Log(inputIP);
+    //}
+
+    //public void SetPlayerName(string inputName)
+    //{
+    //    playerName = inputName;
+    //}
+
+    //public void ConnectToServer()
+    //{
+
+    //    string message = "CONNECT|" + playerName;
+    //    byte[] data = Encoding.UTF8.GetBytes(message);
+    //    Debug.Log("Trying connection");
+    //    Debug.Log(serverIP);
+    //    Debug.Log(playerName);
+    //    //client.Send(data, data.Length, serverEndPoint);
+    //}
+
+    public static void Main()
     {
-        client = new UdpClient();
-        //serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
-    }
+        byte[] data = new byte[1024];
+        string input, stringData;
+        IPEndPoint ipep = new IPEndPoint(
+                        IPAddress.Parse("127.0.0.1"), 9050);
 
-    public void SetIP(string inputIP)
-    {
-        serverIP = inputIP;
-        Debug.Log(inputIP);
-    }
+        Socket server = new Socket(AddressFamily.InterNetwork,
+                       SocketType.Dgram, ProtocolType.Udp);
 
-    public void SetPlayerName(string inputName)
-    {
-        playerName = inputName;
-    }
 
-    public void ConnectToServer()
-    {
-        
-        string message = "CONNECT|" + playerName;
-        byte[] data = Encoding.UTF8.GetBytes(message);
-        Debug.Log("Trying connection");
-        Debug.Log(serverIP);
-        Debug.Log(playerName);
-        //client.Send(data, data.Length, serverEndPoint);
+        string welcome = "Hello, are you there?";
+        data = Encoding.ASCII.GetBytes(welcome);
+        server.SendTo(data, data.Length, SocketFlags.None, ipep);
+
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+        EndPoint Remote = (EndPoint)sender;
+
+        data = new byte[1024];
+        int recv = server.ReceiveFrom(data, ref Remote);
+
+        Console.WriteLine("Message received from {0}:", Remote.ToString());
+        Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+
+        while (true)
+        {
+            input = Console.ReadLine();
+            if (input == "exit")
+                break;
+            server.SendTo(Encoding.ASCII.GetBytes(input), Remote);
+            data = new byte[1024];
+            recv = server.ReceiveFrom(data, ref Remote);
+            stringData = Encoding.ASCII.GetString(data, 0, recv);
+            Console.WriteLine(stringData);
+        }
+        Console.WriteLine("Stopping client");
+        server.Close();
     }
 }
---
+++ Assets/Scripts/Network/UDPServer.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class UDPServer : MonoBehaviour
+{
+    // -------------------------------------------------- FIRST TRY
+    //private UdpClient udpServer;
+    //private int serverPort = 12345; // Choose a port number
+
+    //private void Start()
+    //{
+    //    udpServer = new UdpClient(serverPort);
+    //    Debug.Log("UDP server started on port " + serverPort);
+
+    //    // Start listening for incoming messages in the background
+    //    udpServer.BeginReceive(ReceiveCallback, null);
+    //}
+
+    //private void ReceiveCallback(IAsyncResult result)
+    //{
+    //    IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+    //    byte[] receivedData;
+
+    //    try
+    //    {
+    //        receivedData = udpServer.EndReceive(result, ref clientEndPoint);
+    //        string clientMessage = System.Text.Encoding.UTF8.GetString(receivedData);
+    //        Debug.Log("Received message from " + clientEndPoint + ": " + clientMessage);
+
+    //        // Get the server's name
+    //        string serverName = System.Environment.MachineName;
+
+    //        // Respond to the client with the server's name
+    //        byte[] responseBytes = System.Text.Encoding.UTF8.GetBytes(serverName);
+    //        udpServer.Send(responseBytes, responseBytes.Length, clientEndPoint);
+
+    //        // Continue listening for more messages
+    //        udpServer.BeginReceive(ReceiveCallback, null);
+    //    }
+    //    catch (Exception e)
+    //    {
+    //        Debug.LogError("Error receiving or responding to UDP message: " + e.Message);
+    //    }
+    //}
+
+    //private void OnApplicationQuit()
+    //{
+    //    // Close the UDP socket when the application quits
+    //    udpServer.Close();
+    //}
+
+    public static void Main()
+    {
+        int recv;
+        byte[] data = new byte[1024];
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+
+        Socket newsock = new Socket(AddressFamily.InterNetwork,
+                        SocketType.Dgram, ProtocolType.Udp);
+
+        newsock.Bind(ipep);
+        Console.WriteLine("Waiting for a client...");
+
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+        EndPoint Remote = (EndPoint)(sender);
+
+        recv = newsock.ReceiveFrom(data, ref Remote);
+
+        Console.WriteLine("Message received from {0}:", Remote.ToString());
+        Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+
+        string welcome = "Welcome to my test server";
+        data = Encoding.ASCII.GetBytes(welcome);
+        newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+        while (true)
+        {
+            data = new byte[1024];
+            recv = newsock.ReceiveFrom(data, ref Remote);
+
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+            newsock.SendTo(data, recv, SocketFlags.None, Remote);
+        }
+    }
+}
+
