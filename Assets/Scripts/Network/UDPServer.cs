17f238fe9081610ce1bc84c581022d3130972914
Gerard <gerhoquei8@gmail.com>
2023-10-17 12:00:05 +0200

    udp to threath

--- Assets/Scripts/Network/UDPServer.cs
+++ Assets/Scripts/Network/UDPServer.cs
@@ -1,60 +1,74 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;*/
+
+//public class UDPServer : MonoBehaviour
+
+// -------------------------------------------------- FIRST TRY
+//private UdpClient udpServer;
+//private int serverPort = 12345; // Choose a port number
+
+//private void Start()
+//{
+//    udpServer = new UdpClient(serverPort);
+//    Debug.Log("UDP server started on port " + serverPort);
+
+//    // Start listening for incoming messages in the background
+//    udpServer.BeginReceive(ReceiveCallback, null);
+//}
+
+//private void ReceiveCallback(IAsyncResult result)
+//{
+//    IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+//    byte[] receivedData;
+
+//    try
+//    {
+//        receivedData = udpServer.EndReceive(result, ref clientEndPoint);
+//        string clientMessage = System.Text.Encoding.UTF8.GetString(receivedData);
+//        Debug.Log("Received message from " + clientEndPoint + ": " + clientMessage);
+
+//        // Get the server's name
+//        string serverName = System.Environment.MachineName;
+
+//        // Respond to the client with the server's name
+//        byte[] responseBytes = System.Text.Encoding.UTF8.GetBytes(serverName);
+//        udpServer.Send(responseBytes, responseBytes.Length, clientEndPoint);
+
+//        // Continue listening for more messages
+//        udpServer.BeginReceive(ReceiveCallback, null);
+//    }
+//    catch (Exception e)
+//    {
+//        Debug.LogError("Error receiving or responding to UDP message: " + e.Message);
+//    }
+//}
+
+//private void OnApplicationQuit()
+//{
+//    // Close the UDP socket when the application quits
+//    udpServer.Close();
+//}
+
+using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
 
-public class UDPServer : MonoBehaviour
+public class UDPReceiver : MonoBehaviour
 {
-    // -------------------------------------------------- FIRST TRY
-    //private UdpClient udpServer;
-    //private int serverPort = 12345; // Choose a port number
+    void Start()
+    {
+        Task.Run(() => StartReceiving());
+    }
 
-    //private void Start()
-    //{
-    //    udpServer = new UdpClient(serverPort);
-    //    Debug.Log("UDP server started on port " + serverPort);
-
-    //    // Start listening for incoming messages in the background
-    //    udpServer.BeginReceive(ReceiveCallback, null);
-    //}
-
-    //private void ReceiveCallback(IAsyncResult result)
-    //{
-    //    IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
-    //    byte[] receivedData;
-
-    //    try
-    //    {
-    //        receivedData = udpServer.EndReceive(result, ref clientEndPoint);
-    //        string clientMessage = System.Text.Encoding.UTF8.GetString(receivedData);
-    //        Debug.Log("Received message from " + clientEndPoint + ": " + clientMessage);
-
-    //        // Get the server's name
-    //        string serverName = System.Environment.MachineName;
-
-    //        // Respond to the client with the server's name
-    //        byte[] responseBytes = System.Text.Encoding.UTF8.GetBytes(serverName);
-    //        udpServer.Send(responseBytes, responseBytes.Length, clientEndPoint);
-
-    //        // Continue listening for more messages
-    //        udpServer.BeginReceive(ReceiveCallback, null);
-    //    }
-    //    catch (Exception e)
-    //    {
-    //        Debug.LogError("Error receiving or responding to UDP message: " + e.Message);
-    //    }
-    //}
-
-    //private void OnApplicationQuit()
-    //{
-    //    // Close the UDP socket when the application quits
-    //    udpServer.Close();
-    //}
-
-    public static void Main()
+    private void StartReceiving()
     {
         int recv;
         byte[] data = new byte[1024];
@@ -77,7 +91,6 @@ public class UDPServer : MonoBehaviour
         {
             recv = newsock.ReceiveFrom(data, ref Remote);
 
-
             Console.WriteLine("Message received from {0}:", Remote.ToString());
             Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
 
@@ -97,7 +110,8 @@ public class UDPServer : MonoBehaviour
         {
             Debug.Log("Sin jugadores");
         }
-        
     }
 }
+
+
 
