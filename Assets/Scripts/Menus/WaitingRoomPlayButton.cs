0648eff727476a7af9aa542ef8983eba381dda50
Astrorey776 <astrorey776@gmail.com>
2023-10-24 12:14:40 +0200

    WaitingRoomPlayButton

--- Assets/Scripts/Menus/WaitingRoomPlayButton.cs
+++ Assets/Scripts/Menus/WaitingRoomPlayButton.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WaitingRoomPlayButton : MonoBehaviour
 {
@@ -10,10 +11,10 @@ public class WaitingRoomPlayButton : MonoBehaviour
     {
         if (Input.GetButtonDown("Cancel"))
         {
-            if (serverScript.isClientConnected)
+            /*if (serverScript.isClientConnected)
             {
                 SceneManager.LoadScene("IntroScene");
-            }
+            }*/
         }
     }
 }
