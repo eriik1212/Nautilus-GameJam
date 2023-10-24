2cd063e2212108529c4081aebbcb422018883536
David Boces Obis <79161189+VoZeS@users.noreply.github.com>
2023-10-24 11:39:20 +0200

    Update MessageManager.cs

--- Assets/Scripts/MessageManager.cs
+++ Assets/Scripts/MessageManager.cs
@@ -6,22 +6,31 @@ using UnityEngine.UI;
 
 public class MessageManager : MonoBehaviour
 {
-    // ----------------------------------- Username
-    //private TMP_Text usernameText;
+    // ----------------------------------- Username HOST
+    private TMP_Text usernameHostText;
+    //public TMP_InputField usernameInput;
+
+    // ----------------------------------- Username CLIENT
+    private TMP_Text usernameClientText;
     //public TMP_InputField usernameInput;
 
     // ----------------------------------- IP
-    //private TMP_Text ipText;
+    private TMP_Text ipText;
     //public TMP_InputField ipInput;
 
     // ----------------------------------- ROOM NAME
-    //private TMP_Text roomText;
+    private TMP_Text roomText;
     //public TMP_InputField roomInput;
 
     private Button createButtonUDP;
 
     private void Start()
     {
+        usernameHostText = GameObject.Find("UsernameHostText").GetComponent<TMP_Text>();
+        usernameClientText = GameObject.Find("UsernameClientText").GetComponent<TMP_Text>();
+        ipText = GameObject.Find("ipText").GetComponent<TMP_Text>();
+        roomText = GameObject.Find("RoomNameText").GetComponent<TMP_Text>();
+
         createButtonUDP = GameObject.Find("UDPButton").GetComponent<Button>();
     }
 
@@ -29,32 +38,39 @@ public class MessageManager : MonoBehaviour
     {
         if(createButtonUDP != null)
         {
-            createButtonUDP.onClick.AddListener(SaveUsernameHost);
-            createButtonUDP.onClick.AddListener(SaveUsernameClient);
-            createButtonUDP.onClick.AddListener(SaveRoomName);
-            createButtonUDP.onClick.AddListener(SaveIP);
+            if(usernameHostText != null)
+                createButtonUDP.onClick.AddListener(SaveUsernameHost);
+
+            if(usernameClientText != null)
+                createButtonUDP.onClick.AddListener(SaveUsernameClient);
+
+            if(ipText != null)
+                createButtonUDP.onClick.AddListener(SaveRoomName);
+
+            if(roomText != null)
+                createButtonUDP.onClick.AddListener(SaveIP);
         }
     }
 
     public void SaveUsernameHost()
     {
-        UpdatedText.HostUsernameString = GameObject.Find("UsernameHostText").GetComponent<TMP_Text>().text;
+        UpdatedText.HostUsernameString = usernameHostText.text;
         //Debug.Log("Nombre del host: " + usernameInput.text);
     }
     public void SaveUsernameClient()
     {
-        UpdatedText.ClientUsernameString = GameObject.Find("UsernameClientText").GetComponent<TMP_Text>().text;
+        UpdatedText.ClientUsernameString = usernameClientText.text;
         //Debug.Log("Nombre del cliente: " + usernameInput.text);
     }
 
     public void SaveIP()
     {
-        ClientConnection.ipAdress = GameObject.Find("ipText").GetComponent<TMP_Text>().text;
+        ClientConnection.ipAdress = ipText.text;
         //Debug.Log("IP: " + ipInput.text);
     }
     public void SaveRoomName()
     {
-        UpdatedText.roomNameString = GameObject.Find("RoomNameText").GetComponent<TMP_Text>().text;
+        UpdatedText.roomNameString = roomText.text;
         //Debug.Log("Nombre de la sala: " + roomInput.text);
     }
 }
