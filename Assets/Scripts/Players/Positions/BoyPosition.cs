df1b31ba1816e06fed35e813275e6678e8553f1d
Erik Martín Garzón <erikmarting1200@gmail.com>
2023-11-16 17:30:43 +0100

    player positions handler

---
+++ Assets/Scripts/Players/Positions/BoyPosition.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoyPosition : MonoBehaviour
+{
+    public Transform boyTransform;
+
+    void Start()
+    {
+        
+        
+
+        if (boyTransform == null)
+        {
+            Debug.LogError("No se encontró el objeto del jugador.");
+        }
+    }
+
+    void Update()
+    {
+        // Obtener la posición del jugador
+        Vector3 posicionJugador = boyTransform.position;
+
+        // Puedes imprimir la posición en la consola
+        Debug.Log("Posición del jugador: " + posicionJugador);
+    }
+}
